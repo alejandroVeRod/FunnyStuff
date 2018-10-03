400ed51bccb54cf8ca421a65f3602bb223ba8998
alejandroVerod <alejandro.velasco@alu.uclm.es>
2018-10-03 20:28:50 +0200

    pds

--- test.py
+++ test.py
@@ -1,7 +1,1 @@
-from pynput.mouse import Button, Controller
-
-mouse = Controller()
-
-# Read pointer position
-print('The current pointer position is {0}'.format(
-    mouse.position))
+print('I WANNA LIVEEE')
