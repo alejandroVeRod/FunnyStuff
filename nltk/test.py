d121b0bb9383ebd5a32e800af590df859d968228
alejandroVerod <alejandro.velasco@alu.uclm.es>
2018-12-08 14:24:36 +0100

    nltk

---
+++ nltk/test.py
@@ -0,0 +1,8 @@
+from nltk.tokenize import sent_tokenize,word_tokenize
+
+ex="Hello there, what is your name? My name is Alex, please to meet you."
+
+print(sent_tokenize(ex))
+print(word_tokenize(ex))
+li=word_tokenize(ex)
+print(max(li))
